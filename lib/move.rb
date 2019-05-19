9c783615bab90036e0fd0f27b4e23c75d74c5175
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-19 21:40:13 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,7 @@ def input_to_index(user_input)
   user_input.to_i
 end
 
-def move
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(array, index, value)
+  array[index] = value
 end
